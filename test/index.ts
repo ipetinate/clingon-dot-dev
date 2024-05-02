838d3d9e93d59bd912f990b51a3f574919585998
Isac Petinate <idpetinate@gmail.com>
2024-05-02 11:24:37 -0300

    fix: remove wrapper from test

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,7 @@
 import React, { ReactElement } from 'react'
 import { render, RenderOptions } from '@testing-library/react'
 
-const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
-  return children
-}
-
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
-  render(ui, { wrapper: AllTheProviders, ...options })
+const customRender = (ui: ReactElement, options?: RenderOptions) => render(ui, { ...options })
 
 export * from '@testing-library/react'
 export { customRender as render }
