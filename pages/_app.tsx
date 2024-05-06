66057e1842e7973fb7838a13a218208930598bd4
Isac Petinate <idpetinate@gmail.com>
2024-05-06 15:55:37 -0300

    feat: Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../global.css'
 
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Analytics />
       <SpeedInsights />
 
       <Component {...pageProps} />
