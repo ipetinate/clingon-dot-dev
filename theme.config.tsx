12f3406c4c06c74232ffe3556f35098eb98cd014
Isac Petinate <idpetinate@gmail.com>
2024-05-15 15:26:33 -0300

    feat: add new meta tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,12 +9,27 @@ const config: DocsThemeConfig = {
   chat: {},
   head: (
     <>
+      <link rel="shortcut icon" href="logo/favicon.ico" type="image/x-icon" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Clingon CLI" />
+      <meta name="title" content="Clingon CLI" />
       <meta
         property="og:description"
-        content="Create components, functions, pages, and other files for your project with a few commands"
+        content="Create resources for your project with just a few steps, components, pages, functions, and more"
       />
+      <meta
+        name="description"
+        content="Create resources for your project with just a few steps, components, pages, functions, and more"
+      />
+      <meta
+        name="keywords"
+        content="generator, frontend, clingon, react, vue, developertools, devexp, developerexperience, clitool, toolgenerator, nodejs, vitest, testinglibrary"
+      />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="robots" content="index, follow" />
+      <meta name="language" content="English" />
+      <meta name="revisit-after" content="1 days" />
+      <meta name="author" content="Isac Petinate" />
     </>
   ),
   docsRepositoryBase: 'https://github.com/ipetinate/clingon',
